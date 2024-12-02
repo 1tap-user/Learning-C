dc877e1f5e33444cdd5bcb53b4439bfdaefbf9e7
1tap-user <paudelaaditya910@gmail.com>
2024-12-02 19:50:35 +0545

    Update calculator.c
    little updated

--- calculator.c
+++ calculator.c
@@ -1,29 +1,42 @@
 #include <stdio.h>
-int  main () {
-double num1;
-double num2;
-char o;
-printf ("enter any number");
-scanf("%lf", &num1);
-printf ("enter any operators");
-scanf(" %c", &o);
-printf ("enter any number");
-scanf("%lf", &num2);
 
+int main() {
+    double num1, num2;
+    char operator;
 
-if (o == '+'){
-printf("%f", num1 + num2);
-}
-else if(o == '-'){
-printf("%f",num1 - num2);
-}
-else if (o == '/'){
-printf ("%f", num1 / num2);
-}
-else if (o == '*'){
-printf("%f", num1 * num2);
-}
-else { printf ("invalid operators");
-}
+    // Input first number
+    printf("Enter the first number: ");
+    scanf("%lf", &num1);  
 
+    // Input operator
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &operator);  
+
+    // Input second number
+    printf("Enter the second number: ");
+    scanf("%lf", &num2);
+
+    // Perform calculation based on the operator
+    switch (operator) {
+        case '+':
+            printf("Result: %lf\n", num1 + num2);
+            break;
+        case '-':
+            printf("Result: %lf\n", num1 - num2);
+            break;
+        case '*':
+            printf("Result: %lf\n", num1 * num2);
+            break;
+        case '/':
+            if (num2 != 0) {
+                printf("Result: %lf\n", num1 / num2);
+            } else {
+                printf("Error: Division by zero is not allowed.\n");
+            }
+            break;
+        default:
+            printf("Error: Invalid operator.\n");
+    }
+
+    return 0;
 }
