3b7ccbac5f53df0e54da3a8824a11a303415019d
1tap-user <paudelaaditya910@gmail.com>
2024-12-03 15:42:16 +0000

    loop.c

---
+++ loop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int count = 1;
+
+    // Do-while loop
+    do {
+        printf("I love you\n"); 
+        count++; 
+    } while (count <= 1000); 
+
+    return 0;
+}
