713757563776e3478b84b56550719c43cc139a28
1tap-user <paudelaaditya910@gmail.com>
2024-12-05 04:36:54 +0000

    square.c

---
+++ square.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    double num, square, squareRoot;
+
+    // Input from the user
+    printf("Enter a number: ");
+    scanf("%lf", &num);
+
+    // Calculate square and square root
+    square = num * num;
+    squareRoot = sqrt(num);
+
+    // Display the results
+    printf("Square of %.2lf is %.2lf\n", num, square);
+    printf("Square root of %.2lf is %.2lf\n", num, squareRoot);
+
+    return 0;
+}
